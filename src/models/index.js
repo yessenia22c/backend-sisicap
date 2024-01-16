b39a7a88db74538ee284fb14ee758335a92039c4
Yessenia Villarte Vaca <yesseniavillarte@gmail.com>
2024-01-16 18:40:18 -0400

    configurado para conexion segura 1

--- src/models/index.js
+++ src/models/index.js
@@ -12,7 +12,15 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config, 
+    { host:config.host, 
+      dialect: 'mysql', 
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: true,}
+        }
+      }
+      );
 }
 
 fs
