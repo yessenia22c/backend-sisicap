a6929021cbb250eb827e6a388cb005ec9a03ccb2
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-07-01 19:44:25 -0400

    chore: Update JWT expiration to 8 hours on dist

--- dist/config/config.js
+++ dist/config/config.js
@@ -4,5 +4,5 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.JWT_SECRET = exports.JWT_EXPIRATION = void 0;
-const JWT_EXPIRATION = exports.JWT_EXPIRATION = '1h';
+const JWT_EXPIRATION = exports.JWT_EXPIRATION = '8h';
 const JWT_SECRET = exports.JWT_SECRET = 'Backend';
